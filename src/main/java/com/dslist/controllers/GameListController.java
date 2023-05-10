4992c98925399ce99313f0a5d23634848b8c1da1
lucarauj <lukas.martins193@gmail.com>
2023-05-10 20:55:49 -0300

    Add GET lists

---
+++ src/main/java/com/dslist/controllers/GameListController.java
@@ -0,0 +1,25 @@
+package com.dslist.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dslist.dto.GameListDTO;
+import com.dslist.services.GameListService;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+	
+	@Autowired
+	private GameListService gameListService;
+	
+	@GetMapping
+	public List<GameListDTO> getAll() {
+		return gameListService.findAll();
+	}
+
+}
---
+++ src/main/java/com/dslist/repositories/GameListRepository.java
@@ -0,0 +1,11 @@
+package com.dslist.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dslist.entities.GameList;
+
+@Repository
+public interface GameListRepository extends JpaRepository<GameList, Long>{
+
+}
